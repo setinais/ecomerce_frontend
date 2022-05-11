03ce84df91352e1fb729c06cb18406eaf838bcbc
Vinnicyus Carvalho Gonçalves <vinnicyusgoncalves.prestserv@vibraenergia.com.br>
2022-05-11 08:55:09 -0300

    adiciona layout pagina inicial do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'src/assets/js/main22.js';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ecommerce';
 }
+
